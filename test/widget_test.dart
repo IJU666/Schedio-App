083ab465a882d3f153b6f9713c334d380f3180e5
fachrifatriann <fachri.fatriann10@gmail.com>
2026-01-02 21:40:07 +0700

    antikabupaten

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:reminder_apps/main.dart';
+import 'package:reminder_apps/main2.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
