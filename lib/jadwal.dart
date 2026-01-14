8865b0e6bed82067a8cc938f4e1c1703a5f6c0ea
IJU666 <fauzimaulanabandung2020@gmail.com>
2026-01-14 22:18:12 +0700

    hive

--- lib/jadwal.dart
+++ lib/jadwal.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
 class JadwalScreen extends StatelessWidget {
   const JadwalScreen({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
